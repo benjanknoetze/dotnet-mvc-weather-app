6e37e18eb2cfdbbac1897a023ba9327433793a74
Benjan Knoetze <benjan@codehesion.co.za>
2024-05-30 12:24:00 +0200

    Custom font and response tweaks - Added local custom font, Outfit - Responsive tweaks - Refined styling

--- WeatherApp/tailwind.config.js
+++ WeatherApp/tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './Views/**/*.cshtml',
@@ -8,7 +10,14 @@ module.exports = {
     extend: {
       boxShadow: {
         'custom-light': '#0169aa69 0 0 20px -10px',
-      }
+        'custom-hover': '#0169aab7 0 0 10px -5px',
+      },
+      fontFamily: {
+        'sans': ['"Outfit"', ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        'sm': {'max': '639px'},
+      },
     },
   },
   plugins: [],
